2f623e461ca78ff517728b1eb2ccd0f0a92ecc4a
theAxels <angkianto.laurence@binus.ac.id>
2024-01-26 22:42:58 +0700

    update users 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email')->nullable();
             $table->text('profile_pic');
             $table->string('password');
-            // $table->rememberToken();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
