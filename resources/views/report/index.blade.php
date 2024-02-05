c220d128976e3ec07d5198952980e3ddd933b342
Agustinus <57866141+lgarin211@users.noreply.github.com>
2024-02-05 14:48:12 +0000

    2

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -16,10 +16,14 @@
     <thead>
         <tr>
             <th>ID</th>
+            <th>Name</th>
             <th>Title</th>
             <th>Time</th>
             <th>Evidence</th>
             <th>Category</th>
+            <th>Category name</th>
+            <th>IsProcess</th>
+            <th>IsDone</th>
             <th>Action</th>
         </tr>
     </thead>
@@ -28,9 +32,14 @@
         @foreach($reports as $report)
             <tr>
                 <td>{{ $report->id}}</td>
+                @php
+                    $name = DB::table('users')->where('id', $report->user_id)->value('name')
+                @endphp
+                <td>{{ $name }}</td>
                 <td>{{ $report->title}}</td>
                 <td>{{ $report->created_at}}</td>
                 <td>{{ $report->evidence }}</td>
+                <td>{{ $report->category_id }}</td>
                 
 
                 @php
@@ -39,8 +48,12 @@
                 
                 <td>{{ $category_name }}</td>
 
+                <td>{{ $report->IsProcess ? 'Yes' : 'No' }}</td>
+                <td>{{ $report->isDone ? 'Yes' : 'No' }}</td>
+
                 <td>
                     <a href="{{ route('report.edit', ['report' => $report]) }}" class="btn btn-primary">Edit</a>
+                    <a href="{{ route('dashboard.detail', ['report' => $report]) }}" class="btn btn-info">View Details</a>
                     <form method="post" action="{{ route('report.destroy', ['report' => $report]) }}" style="display: inline-block;">
                         @csrf
                         @method('delete')
